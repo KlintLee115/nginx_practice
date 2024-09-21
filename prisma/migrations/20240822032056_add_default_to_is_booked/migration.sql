c89855d4aa5609c2bdf16b3975f4fd824e8155cc
KlintLee115 <klintlee1@gmail.com>
2024-09-21 16:55:40 -0600

    simplicity for practice

--- prisma/migrations/20240822032056_add_default_to_is_booked/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `is_booked` on table `seats` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "seats" ALTER COLUMN "is_booked" SET NOT NULL,
-ALTER COLUMN "is_booked" SET DEFAULT false;
--- src/utils/functions.ts
+++
@@ -1,3 +0,0 @@
-export function IsNullOrUndefined(element: any) {
-    return element === undefined || element === null
-}
